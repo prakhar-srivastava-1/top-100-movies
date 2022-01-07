217bf655c3ec6158edb07eb17f11f7f828b7e015
Prakhar Srivastava <prakhar.srivastava.2409@gmail.com>
2022-01-07 06:16:18 +0530

    finish adding logic to grab and print the movies

--- main.py
+++ main.py
@@ -10,7 +10,9 @@ webpage = response.text
 # make soup
 soup = BeautifulSoup(webpage, "html.parser")
 
-# debug
-print(soup)
+# get all movie titles - h3 class="title"
+all_movies = soup.find_all(name="h3", class_="title")
 
-
+# print title and name of the movie
+for movie in all_movies[::-1]:
+    print(movie.getText())
